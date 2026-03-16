d8a4b279042298b52128c80b93c061dac48ff1f6
jsromagnoli <josuesilvaromagnollo@gmail.com>
2026-03-16 20:31:32 -0300

    fix: corrige erros de lint no app.py

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-import os                           # importa o modulo nativo do python ( interação com SO)
-from flask import Flask, jsonify    # Importa o Flask (o framework web) e o jsonify (função que converte um dicionário Python em resposta JSON)
-from dotenv import load_dotenv      #Importa a função que lê o arquivo .env e carrega as variáveis
+import os
+from flask import Flask, jsonify
+from dotenv import load_dotenv
 
-load_dotenv()  # carrega o .env automaticamente
+load_dotenv()
 
-app = Flask(__name__)               #Lê o arquivo .env e carrega APP_ENV=development
+app = Flask(__name__)
 
-@app.route("/health")               #diz ao Flask: "quando alguém acessar /health, execute a função abaixo"
-def health():                       #Define a função que será executada quando o endpoint for chamado
+
+@app.route("/health")
+def health():
     return jsonify({
         "status": "ok",
         "version": "1.0.0",
         "environment": os.getenv("APP_ENV", "development")
     }), 200
 
-if __name__ == "__main__":          # só executa se você rodar o arquivo diretamente (python app.py), e não quando importado por outro arquivo
-    app.run(host="0.0.0.0", port=8080) #aceita conexões de qualquer endereço (essencial para Docker)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
